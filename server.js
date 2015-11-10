c80cf94a1e9881629e4ffc17277ba2fe6e0a29b5
ariffurqon <arif.furqon@gmail.com>
2015-11-09 21:57:56 -0800

    updated the views and server

--- server.js
+++ server.js
@@ -1,17 +1,45 @@
 // require express framework and additional modules
-var express = require('express'),
-    app = express(),
-    bodyParser = require('body-parser');
+var express = require('express');
+var exphbs  = require('express-handlebars');
+var moment = require('moment');
+var bodyParser = require('body-parser');
+var instagram = require('instagram-node-lib');
+var app = express();
+
+    
 
 // tell app to use bodyParser middleware
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-// set up root route to respond with 'hello world'
+// set the file name of the default layout
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
+// set the expressJS view engine to handlebars
+app.set('view engine', 'handlebars');
+
+// set the path to the front-end assets
+app.use(express.static('public'));
+
+var instagram_client_id = 'baacf005bd4f4a758534cce1fc8d69b4';
+var instagram_client_secret ='e08f3e900c1e4f83859b2ecc0643568f';
+
+
+instagram.set('client_id', instagram_client_id);
+instagram.set('client_secret', instagram_client_secret);
+
+
+var io = require('socket.io').listen(server);
+
+
 app.get('/', function (req, res) {
-  res.send('hello lovers..');
+  res.render('home');
 });
 
-// listen on port 3000
-app.listen(3000, function () {
-  console.log('server started on localhost:3000');
+// listen on port 4000
+var server = app.listen(4000, function(){
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('listening at http://%s:%s', host, port);
 });
